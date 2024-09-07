eff19b8f974550cac12c575595792ae0bfe06406
Ruslan <Giovingo92@gmail.com>
2024-09-07 18:02:11 +0300

    feat: arrays types

--- app.ts
+++ app.ts
@@ -5,6 +5,15 @@
 const person = {
 	name: 'Ruslan',
 	age: 32,
+	hobbies: ['Sports', 'Cooking'],
 }
 
+let favoriteActivities: string[]
+favoriteActivities = ['Sports']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+	console.log(hobby.toUpperCase())
+	// console.log(hobby.map()) !!! ERROR
+}
