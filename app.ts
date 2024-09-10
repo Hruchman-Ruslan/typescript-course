733568d3160171b9d13ebc5b9f45cb832f99eb52
Ruslan <Giovingo92@gmail.com>
2024-09-10 18:58:31 +0300

    feat: never type

--- app.ts
+++ app.ts
@@ -9,3 +9,10 @@ if (typeof userInput === 'string') {
 }
 
 // userName = userInput no Error
+
+function generateError(message: string, code: number): never {
+	throw { message: message, errorCode: code }
+	// while(true) {}
+}
+
+generateError('An error occurred!', 500)
