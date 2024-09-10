cdb931e31256830d6ee006b6831ab705887393dd
Ruslan <Giovingo92@gmail.com>
2024-09-10 18:05:16 +0300

    feat: function type calldack

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
 	console.log('Result:' + num)
 }
 
+function addAndHandler(n1: number, n2: number, cd: (num: number) => void) {
+	const result = n1 + n2
+	cd(result)
+}
+
 printResult(add(5, 12))
 
 let combineValues: (a: number, b: number) => number
@@ -17,3 +22,7 @@ combineValues = add
 console.log(combineValues(8, 8))
 
 // let someValue: undefined
+
+addAndHandler(10, 20, result => {
+	console.log(result)
+})
