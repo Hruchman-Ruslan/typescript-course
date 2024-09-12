058a7c0419a5e89bf168103bb2c119f0c436a69a
Ruslan <Giovingo92@gmail.com>
2024-09-12 20:15:41 +0300

    feat: code quality options

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
+let appId = 'adc'
+
 const button = document.querySelector('button')!
 
-function clickHandler(message: string) {
+function clickHandler(message: string, age: number) {
+	let userName = 'Ruslan'
 	console.log('Clicked!' + message)
 }
 
+function add(n1: number, n2: number) {
+	if (n1 + n2 > 0) {
+		return n1 + n2
+	}
+	// return
+}
+
 if (button) {
-	button.addEventListener('click', clickHandler.bind(null, " You're welcome"))
+	button.addEventListener(
+		'click',
+		clickHandler.bind(null, " You're welcome", 32)
+	)
 }
