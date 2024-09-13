7c963d4e863ee2ee0fca39b86f74435455da93fc
Ruslan <Giovingo92@gmail.com>
2024-09-13 17:46:46 +0300

    feat: rest

--- src/app.ts
+++ src/app.ts
@@ -19,15 +19,15 @@ age = 33
 // console.log(isOld)
 // console.log(result)
 
-const add = (a: number, b: number = 5) => a + b
+// const add = (a: number, b: number = 5) => a + b
 
-const printOutput: (a: number | string) => void = output => console.log(output)
+// const printOutput: (a: number | string) => void = output => console.log(output)
 
-const button = document.querySelector('button')
+// const button = document.querySelector('button')
 
-button?.addEventListener('click', e => console.log(e))
+// button?.addEventListener('click', e => console.log(e))
 
-printOutput(add(2))
+// printOutput(add(2))
 
 const hobbies = ['Sport', 'Cookies']
 const activeHobbies = ['Hiking']
@@ -40,4 +40,23 @@ const person = {
 }
 
 const copiedPerson = { ...person }
+
 console.log(copiedPerson)
+
+// const add = (...numbers: [number, number, number]) => {  if need 3 parameters
+// 	return numbers.reduce((curtResult, curtValue) => {
+// 		return curtResult + curtValue
+// 	}, 0)
+// }
+
+// const addedNumbers = add(5, 10, 2)  if need 3 parameters
+
+const add = (...numbers: number[]) => {
+	return numbers.reduce((curtResult, curtValue) => {
+		return curtResult + curtValue
+	}, 0)
+}
+
+const addedNumbers = add(5, 10, 2, 3.7)
+
+console.log(addedNumbers)
