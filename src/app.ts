14f64291457b8167a17c26128864d8375ad6ef0b
Ruslan <Giovingo92@gmail.com>
2024-09-13 11:35:09 +0300

    feat: let const var ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-// reset
+const useName = 'Ruslan'
+// userName = "Other" ERROR
+let age = 32
+age = 33
+
+// var result
+
+function add(a: number, b: number) {
+	// var result No find console.log(result)
+	let result
+	result = a + b
+	return result
+}
+
+// if (age > 20) {
+// 	let isOld = true
+// }
+
+// console.log(isOld)
+// console.log(result)
