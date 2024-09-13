61439d195233b72771e241ee54eef9aec6b7ce42
Ruslan <Giovingo92@gmail.com>
2024-09-13 13:39:19 +0300

    feat: arrow function

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ age = 33
 
 // var result
 
-function add(a: number, b: number) {
-	// var result No find console.log(result)
-	let result
-	result = a + b
-	return result
-}
+// function add(a: number, b: number) {
+// 	// var result No find console.log(result)
+// 	let result
+// 	result = a + b
+// 	return result
+// }
 
 // if (age > 20) {
 // 	let isOld = true
@@ -18,3 +18,13 @@ function add(a: number, b: number) {
 
 // console.log(isOld)
 // console.log(result)
+
+const add = (a: number, b: number) => a + b
+
+const printOutput: (a: number | string) => void = output => console.log(output)
+
+const button = document.querySelector('button')
+
+button?.addEventListener('click', e => console.log(e))
+
+printOutput(add(2, 5))
