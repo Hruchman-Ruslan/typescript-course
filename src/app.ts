e108cbc7b6a56cf6375827c76d73bcc517908cf8
Ruslan <Giovingo92@gmail.com>
2024-09-14 18:11:32 +0300

    feat: privet and public

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-	name: string
+	public name: string // public it's default properties
+	private employees: string[] = []
 
 	constructor(n: string) {
 		this.name = n
@@ -8,10 +9,27 @@ class Department {
 	describe(this: Department) {
 		console.log('Department ' + this.name)
 	}
+
+	addEmployee(employee: string) {
+		this.employees.push(employee)
+	}
+
+	printEmployeeInformation() {
+		console.log(this.employees.length)
+		console.log(this.employees)
+	}
 }
 
 const accounting = new Department('Accounting')
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
+
+accounting.addEmployee('Ruslan')
+accounting.addEmployee('Max')
+
+// accounting.employees[2] = 'Anna' is Error because private employees
+accounting.name = 'NEW NAME' // we can change because it's public properties
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
 
 accounting.describe()
-accountingCopy.describe()
+accounting.printEmployeeInformation()
+// accountingCopy.describe()
