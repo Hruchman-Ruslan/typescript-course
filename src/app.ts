df995f6a4b23ab667dd0e06273fd2c54c5999a1d
Ruslan <Giovingo92@gmail.com>
2024-09-14 17:35:23 +0300

    feat: contructor fun & this

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,14 @@ class Department {
 	constructor(n: string) {
 		this.name = n
 	}
+
+	describe(this: Department) {
+		console.log('Department ' + this.name)
+	}
 }
 
 const accounting = new Department('Accounting')
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
 
-console.log(accounting)
+accounting.describe()
+accountingCopy.describe()
