a74a543c64e17edeb84eb4d3be4f897dfed73876
Ruslan <Giovingo92@gmail.com>
2024-09-16 17:30:13 +0300

    feat: inheritance

--- src/app.ts
+++ src/app.ts
@@ -23,15 +23,43 @@ class Department {
 	}
 }
 
-const accounting = new Department('d1', 'Accounting')
+class ITDepartment extends Department {
+	admins: string[]
+	constructor(id: string, admins: string[]) {
+		super(id, 'IT')
+		this.admins = admins
+	}
+}
 
-accounting.addEmployee('Ruslan')
-accounting.addEmployee('Max')
+class AccountingDepartment extends Department {
+	constructor(id: string, private reports: string[]) {
+		super(id, 'Accounting')
+	}
 
-// accounting.employees[2] = 'Anna' is Error because private employees
-// accounting.name = 'NEW NAME' // we can change because it's public properties
+	addReport(text: string) {
+		this.reports.push(text)
+	}
+
+	printReports() {
+		console.log(this.reports)
+	}
+}
+
+const it = new ITDepartment('d1', ['Ruslan'])
+const accounting = new AccountingDepartment('d2', [])
+
+it.addEmployee('Ruslan')
+it.addEmployee('Max')
+
+console.log(it)
+
+// it.employees[2] = 'Anna' is Error because private employees
+// it.name = 'NEW NAME' // we can change because it's public properties
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
 
-accounting.describe()
-accounting.printEmployeeInformation() // accountingCopy.describe()
+it.describe()
+it.printEmployeeInformation() // accountingCopy.describe()
+
+accounting.addReport('Something went wrong...')
+accounting.printReports()
