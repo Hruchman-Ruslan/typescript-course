9e6e0d0df9a421df81760f3f6d1cb14d98f51b77
Ruslan <Giovingo92@gmail.com>
2024-09-16 17:44:12 +0300

    protected

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 class Department {
 	// public readonly id: string // public it's default properties
 	// public name: string // public it's default properties
-	private employees: string[] = []
+	// private employees: string[] = [] // can use inside Department class
+	protected employees: string[] = [] // can use in extends Classes example ITDepartment and AccountingDepartment
 
 	constructor(private readonly id: string, public name: string) {
 		// this.name = name
@@ -36,6 +37,13 @@ class AccountingDepartment extends Department {
 		super(id, 'Accounting')
 	}
 
+	addEmployee(name: string) {
+		if (name === 'Ruslan') {
+			return
+		}
+		this.employees.push(name)
+	}
+
 	addReport(text: string) {
 		this.reports.push(text)
 	}
@@ -46,20 +54,26 @@ class AccountingDepartment extends Department {
 }
 
 const it = new ITDepartment('d1', ['Ruslan'])
-const accounting = new AccountingDepartment('d2', [])
 
 it.addEmployee('Ruslan')
 it.addEmployee('Max')
 
-console.log(it)
-
 // it.employees[2] = 'Anna' is Error because private employees
-// it.name = 'NEW NAME' // we can change because it's public properties
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
 
 it.describe()
+it.name = 'NEW NAME' // we can change because it's public properties
 it.printEmployeeInformation() // accountingCopy.describe()
 
+console.log(it)
+
+const accounting = new AccountingDepartment('d2', [])
+
 accounting.addReport('Something went wrong...')
+
+accounting.addEmployee('Ruslan')
+accounting.addEmployee('Max')
+
 accounting.printReports()
+accounting.printEmployeeInformation()
