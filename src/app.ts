190da167da89a3c6b6af9e0cb77c3aefe0ad77a6
Ruslan <Giovingo92@gmail.com>
2024-09-16 19:30:31 +0300

    feat: getters and setters

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+	private lastReports: string
+
+	get mostRecentReport() {
+		if (this.lastReports) {
+			return this.lastReports
+		}
+		throw new Error('No report found.')
+	}
+
+	set mostRecentReport(value: string) {
+		if (!value) {
+			throw new Error('Please pass in a valid value!')
+		}
+		this.addReport(value)
+	}
+
 	constructor(id: string, private reports: string[]) {
 		super(id, 'Accounting')
+		this.lastReports = reports[0]
 	}
 
 	addEmployee(name: string) {
@@ -46,6 +63,7 @@ class AccountingDepartment extends Department {
 
 	addReport(text: string) {
 		this.reports.push(text)
+		this.lastReports = text
 	}
 
 	printReports() {
@@ -70,7 +88,9 @@ console.log(it)
 
 const accounting = new AccountingDepartment('d2', [])
 
+accounting.mostRecentReport = 'Year End Report'
 accounting.addReport('Something went wrong...')
+console.log(accounting.mostRecentReport)
 
 accounting.addEmployee('Ruslan')
 accounting.addEmployee('Max')
