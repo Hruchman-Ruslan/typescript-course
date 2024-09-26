2b2318747cfe37a6c8b69b9b7d4b01f648d3ec02
Ruslan <Giovingo92@gmail.com>
2024-09-26 16:14:57 +0300

    feat: built-in generic

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-// reset
+// const names: Array<string> = [] // string[]
+// // names[0].split(' ')
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		resolve(10)
+// 	}, 2000)
+// })
+
+// promise.then(data => {
+// 	// data.split(' ') error because Promise<number>
+// })
