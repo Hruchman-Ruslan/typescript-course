3951f8f9bab00577c0a4cd2a125be2ce87ac3ef8
Ruslan <Giovingo92@gmail.com>
2024-09-26 16:34:46 +0300

    feat: creating a generic function

--- src/app.ts
+++ src/app.ts
@@ -10,3 +10,13 @@
 // promise.then(data => {
 // 	// data.split(' ') error because Promise<number>
 // })
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+	return Object.assign(objA, objB)
+}
+
+// same generic types T and U
+const mergeObject = merge<{ name: string; hobbies: string[] }, { age: number }>(
+	{ name: 'Ruslan', hobbies: ['Sports'] },
+	{ age: 32 }
+)
