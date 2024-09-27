d4bd58f6386e25f6d9a3e7ce9b2f0aa05314f3b0
Ruslan <Giovingo92@gmail.com>
2024-09-27 20:20:14 +0300

    feat: another generic func

--- src/app.ts
+++ src/app.ts
@@ -20,3 +20,23 @@ const mergeObject = merge<{ name: string; hobbies: string[] }, { age: number }>(
 	{ name: 'Ruslan', hobbies: ['Sports'] },
 	{ age: 32 }
 )
+
+console.log(mergeObject)
+
+interface Lengthy {
+	length: number
+}
+
+function countAndDescribe<T extends Lengthy>(el: T): [T, string] {
+	let descriptionText = 'Got not value.'
+	if (el.length === 1) {
+		descriptionText = 'Got 1 element.'
+	} else if (el.length > 1) {
+		descriptionText = 'Got ' + el.length + ' elements.'
+	}
+	return [el, descriptionText]
+}
+
+// console.log(countAndDescribe('Hi there!')) // 'Hi there!', 'Got 9 elements.'
+console.log(countAndDescribe(['Sports', 'Hobbies'])) // [Array(2), 'Got 2 elements.']
+// console.log(countAndDescribe(10)) // error
