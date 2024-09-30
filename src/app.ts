36c78dcc08badd9e16509060276d212115a57f81
Ruslan <Giovingo92@gmail.com>
2024-09-30 17:56:07 +0300

    feat: a first class decorator

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-// reset
+function Logger(construction: Function) {
+	console.log('Logging...')
+	console.log(construction)
+}
+
+@Logger
+class Person {
+	name = 'Ruslan'
+
+	constructor() {
+		console.log('Creating person object...')
+	}
+}
+
+const per = new Person()
+
+console.log(per)
