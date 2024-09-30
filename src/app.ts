a2fecd0498b74b0de42cd86665ac86bf207063a6
Ruslan <Giovingo92@gmail.com>
2024-09-30 20:18:09 +0300

    feat: building more useful decorators

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,19 @@ function Logger(logString: string) {
 	}
 }
 
-@Logger('LOGGING - PERSON')
+function WithTemplate(template: string, hookId: string) {
+	return function (construction: any) {
+		const element = document.getElementById(hookId)
+		const p = new construction()
+		if (element) {
+			element.innerHTML = template
+			element.querySelector('h1')!.textContent = p.name
+		}
+	}
+}
+
+// @Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object<h1/>', 'app')
 class Person {
 	name = 'Ruslan'
 
