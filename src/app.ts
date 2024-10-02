0be3c9c50957111f8f639693e5626ea0767d8942
Ruslan <Giovingo92@gmail.com>
2024-10-02 17:51:26 +0300

    feat: validation with decorators

--- src/app.ts
+++ src/app.ts
@@ -127,11 +127,54 @@ button.addEventListener('click', p.showMessage) // line 106
 
 // ---
 
-function Required() {}
+interface ValidatorConfig {
+	[properties: string]: {
+		[validatableProp: string]: string[] // [ 'required', 'positive']
+	}
+}
 
-function PositiveNumber() {}
+const registeredValidators: ValidatorConfig = {}
 
-function validate(obj: object) {}
+function Required(target: any, propName: string) {
+	registeredValidators[target.constructor.name] = {
+		...registeredValidators[target.constructor.name],
+		[propName]: [
+			...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+			'required',
+		],
+	}
+}
+
+function PositiveNumber(target: any, propName: string) {
+	registeredValidators[target.constructor.name] = {
+		...registeredValidators[target.constructor.name],
+		[propName]: [
+			...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+			'positive',
+		],
+	}
+}
+
+function validate(obj: any) {
+	const objValidatorConfig = registeredValidators[obj.constructor.name]
+	if (!objValidatorConfig) {
+		return true
+	}
+	let isValid = true
+	for (const prop in objValidatorConfig) {
+		for (const validator of objValidatorConfig[prop]) {
+			switch (validator) {
+				case 'required':
+					isValid = isValid && !!obj[prop]
+					break
+				case 'positive':
+					isValid = isValid && obj[prop] > 0
+					break
+			}
+		}
+	}
+	return isValid
+}
 
 class Course {
 	@Required
@@ -157,7 +200,10 @@ courseFrom.addEventListener('submit', e => {
 
 	const createCourse = new Course(title, price)
 
-	validate(createCourse)
+	if (!validate(createCourse)) {
+		alert('Invalid input, please try again!')
+		return
+	}
 
 	console.log('createCourse', createCourse)
 })
